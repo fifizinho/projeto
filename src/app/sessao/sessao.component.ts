e08b1b3f0ead1368308b0b76d6eede6286c91630
fifizinho <132359775+fifizinho@users.noreply.github.com>
2023-11-14 14:24:47 -0300

    projeto super mercado

--- src/app/sessao/sessao.component.ts
+++ src/app/sessao/sessao.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sessao',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./sessao.component.scss']
 })
 export class SessaoComponent {
-
+  Comprar(){
+    Swal.fire({
+      title: "Vc deseja escolher ela mesmo?",
+      showDenyButton: true,
+      icon: "question",
+      confirmButtonText: "Sim",
+      denyButtonText: `Cancelar`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Artista escolhido com sucesso!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Erro ao escolher o Artista", "", "info");
+      }
+    });
+  }
 }
