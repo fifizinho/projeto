d000c2d678a75568f6e56c0630b3bd59f9f0f489
fifizinho <132359775+fifizinho@users.noreply.github.com>
2023-11-21 16:36:07 -0300

    project

--- src/app/sessao/sessao.component.ts
+++ src/app/sessao/sessao.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
+import { OnInit} from '@angular/core'
+import { SessaoService } from './service/sessao.service';
+import { Isessao } from './service/isessao';
 
 @Component({
   selector: 'app-sessao',
   templateUrl: './sessao.component.html',
   styleUrls: ['./sessao.component.scss']
 })
-export class SessaoComponent {
+export class SessaoComponent implements OnInit{
+ngOnInit(): void {
+    this.listar()
+}
+  produtos: Isessao[] = [];
+
+  constructor(private service:SessaoService){}
+
+  listar(){
+    this.service.listar().subscribe(dados => this.produtos = dados)
+  }
+
   Comprar(){
     Swal.fire({
       title: "Vc deseja comprar esse produto?",
